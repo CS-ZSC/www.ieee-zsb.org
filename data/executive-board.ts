11f71338664d2d8aeecfa15cce82e6aaa77c444c
hulxv <hulxxv@gmail.com>
2026-01-29 01:01:15 +0200

    data: update executive board

--- data/executive-board.ts
+++ data/executive-board.ts
@@ -1,48 +1,98 @@
 import type { Position } from "./position";
 
+
+// 2025
+// const executiveBoard: Position[] = [
+//   {
+//     name: "Shahd Elbendary",
+//     position: "Chairperson",
+//     avatarSrc: "/Images/board/executive-board/chairperson.webp",
+//     linkedin: "https://www.linkedin.com/in/shahd-elbendary",
+//     email: "shahd.elbendary@ieee-zsb.org",
+//   },
+//   {
+//     name: "Amr Yasser Saber",
+//     position: "Vice Technical Chairperson",
+//     avatarSrc: "/Images/board/executive-board/vice-technical.webp",
+//     linkedin: "https://www.linkedin.com/in/amr-yasser-559b59228",
+//     email: "amr.yasser@ieee-zsb.org",
+//   },
+//   {
+//     name: "Mustafa Mohamed",
+//     position: "Vice Managerial Chairperson",
+//     avatarSrc: "/Images/board/executive-board/vice-managerial.webp",
+//     linkedin: "https://www.linkedin.com/in/mustafa-mohamed-",
+//     email: "mustafa.mohammed@ieee-zsb.org",
+//   },
+//   {
+//     name: "Samar Nafea",
+//     position: "Secretary",
+//     avatarSrc: "/Images/board/executive-board/secretary.webp",
+//     linkedin: "https://www.linkedin.com/in/samar-nafea",
+//     email: "samar.nafea@ieee-zsb.org",
+//   },
+//   {
+//     name: "Mohamed Tarek",
+//     position: "Branding Officer",
+//     avatarSrc: "/Images/board/executive-board/branding-officer.webp",
+//     linkedin: "https://www.linkedin.com/in/mohamed-tarek-abdelazim",
+//     email: "mohamed.tarek@ieee-zsb.org",
+//   },
+//   {
+//     name: "Sarah Kahwash",
+//     position: "Treasurer",
+//     avatarSrc: "/Images/board/executive-board/treasurer.webp",
+//     linkedin: "https://www.linkedin.com/in/sara-kahwash-56023a269",
+//     email: "sarah.kahwash@ieee-zsb.org",
+//   },
+// ];
+
+// 2026
 const executiveBoard: Position[] = [
   {
-    name: "Shahd Elbendary",
+    name: "Ahmed Raiyah",
     position: "Chairperson",
     avatarSrc: "/Images/board/executive-board/chairperson.webp",
-    linkedin: "https://www.linkedin.com/in/shahd-elbendary",
-    email: "shahd.elbendary@ieee-zsb.org",
+    linkedin: "https://www.linkedin.com/in/ahmed-raiyah",
+    email: "ahmed.raiyah@ieee-zsb.org",
   },
   {
-    name: "Amr Yasser Saber",
-    position: "Vice Technical Chairperson",
+    name: "Ahmed Medhat",
+    position: "Technical Vice Chairperson",
     avatarSrc: "/Images/board/executive-board/vice-technical.webp",
-    linkedin: "https://www.linkedin.com/in/amr-yasser-559b59228",
-    email: "amr.yasser@ieee-zsb.org",
+    linkedin: "https://www.linkedin.com/in/ahmed-medhat-212847271",
+    email: "ahmed.medhat@ieee-zsb.org",
   },
   {
-    name: "Mustafa Mohamed",
-    position: "Vice Managerial Chairperson",
+    name: "Mohamed Abdalaziz",
+    position: "Managerial Vice Chairperson",
     avatarSrc: "/Images/board/executive-board/vice-managerial.webp",
-    linkedin: "https://www.linkedin.com/in/mustafa-mohamed-",
-    email: "mustafa.mohammed@ieee-zsb.org",
+    linkedin: "https://www.linkedin.com/in/mohamed-abdalaziz-hussien/",
+    email: "mohamed.abdalaziz@ieee-zsb.org",
   },
   {
-    name: "Samar Nafea",
+    name: "Mohamed Emad",
+    position: "Webmaster",
+    avatarSrc: "/Images/board/executive-board/webmaster.webp",
+    linkedin: "https://www.linkedin.com/in/hulxv",
+    email: "mohamed.elsawy@ieee-zsb.org",
+  },
+  {
+    name: "Engy Mohamed",
     position: "Secretary",
     avatarSrc: "/Images/board/executive-board/secretary.webp",
-    linkedin: "https://www.linkedin.com/in/samar-nafea",
-    email: "samar.nafea@ieee-zsb.org",
+    linkedin: "https://linkedin.com/in/engy-mohammed-54706a336",
+    email: "engy.mohamed@ieee-zsb.org",
   },
   {
-    name: "Mohamed Tarek",
-    position: "Branding Officer",
-    avatarSrc: "/Images/board/executive-board/branding-officer.webp",
-    linkedin: "https://www.linkedin.com/in/mohamed-tarek-abdelazim",
-    email: "mohamed.tarek@ieee-zsb.org",
-  },
-  {
-    name: "Sarah Kahwash",
+    name: "Ahmed Elnaggar",
     position: "Treasurer",
     avatarSrc: "/Images/board/executive-board/treasurer.webp",
-    linkedin: "https://www.linkedin.com/in/sara-kahwash-56023a269",
-    email: "sarah.kahwash@ieee-zsb.org",
+    linkedin: "https://www.linkedin.com/in/ahmedelnaggarr",
+    email: "ahmed.elnaggar@ieee-zsb.org",
   },
 ];
+
+
 
 export default executiveBoard;
